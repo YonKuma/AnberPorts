4be1b62c9ff0a4323a8407c5ba287cfb12997d60
YonKuma <nate.skiba@gmail.com>
2021-06-16 21:12:47 -0400

    Manually loading libpng library for srb2kart

--- misc/Sonic Robo Blast 2 Kart.sh
+++ misc/Sonic Robo Blast 2 Kart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/srb2kart"
+LIB64="$(dirname `pwd`)/AnberPorts/lib64"
 export SDL_GAMECONTROLLERCONFIG="$(cat gamecontrollerdb.txt)"
+export LD_PRELOAD="$LIB64/libpng16.so"
 ./srb2kart 2>&1 | tee -a ./log.txt
 unset SDL_GAMECONTROLLERCONFIG
